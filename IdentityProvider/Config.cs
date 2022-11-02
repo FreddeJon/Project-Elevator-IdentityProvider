d968ec393b3321297f229a652d0f449b5a102abb
Fredrik <fredrik.jonson@student.kyh.se>
2022-11-02 18:25:39 +0100

    OIDC Config
    Added configs for localhost client to be able to test

--- IdentityProvider/Config.cs
+++ IdentityProvider/Config.cs
@@ -1,23 +1,72 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityProvider
 {
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
+               new IdentityResource[]
+               {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource("roles", "Your roles(s)", new []{ "role"})
+               };
+
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+        new("projectelevatorapi", "Project - Elevator API", new []{"role"})
+        {
+            Scopes =
             {
-            new IdentityResources.OpenId()
-            };
-        public static IEnumerable<ApiResource> ApiResources => 
-            new ApiResource[]
-                { };
+                "projectelevatorapi.fullaccess",
+                "projectelevatorapi.write",
+                "projectelevatorapi.read"
+            }
+        }
+        };
+
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
-                { };
+            {
+            new ApiScope("projectelevatorapi.fullaccess"),
+            new ApiScope("projectelevatorapi.write"),
+            new ApiScope("projectelevatorapi.read")
+            };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
-                { };
+            {
+            new Client
+            {
+                ClientName = "Localhost AdminWebApp Client",
+                ClientId = "adminwebappclient",
+                AllowedGrantTypes = GrantTypes.Code,
+                AllowOfflineAccess = true,
+                UpdateAccessTokenClaimsOnRefresh = true,
+                RedirectUris =
+                {
+                    "https://localhost:7196/signin-oidc"
+                },
+                PostLogoutRedirectUris =
+                {
+                    "https://localhost:7196/signout-callback-oidc"
+                },
+
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "roles",
+                    "projectelevatorapi.read",
+                    "projectelevatorapi.write",
+                },
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                RequireConsent = false
+            }
+            };
     }
 }
